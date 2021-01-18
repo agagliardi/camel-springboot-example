7102edbdc06f69dd6ee4ebbb436a13ffb7b26e17
Antonio Gagliardi <antonio.gagliardi@gmail.com>
2021-01-18 15:49:07 +0100

    initial

---
+++ src/main/java/com/redhat/reproducer/MySpringBootRouter.java
@@ -0,0 +1,20 @@
+package com.redhat.reproducer;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints
+ * to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer://foo?fixedRate=true&period=1000").routeId("javaDSL").to("log:hello from Java DSL");
+    }
+
+}
